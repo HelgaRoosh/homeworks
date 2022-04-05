a970507666a68af0f5e0aeb15ce69f7f2efd5b9f
HelgaRoosh <vulfina@mail.ru>
2022-04-05 22:51:47 +0500

    edit solveEquation

--- task.js
+++ task.js
@@ -1,6 +1,13 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let diskr = b ** 2 - 4 * a * c;
+  if (diskr > 0) {
+    arr[0] = (-b + Math.sqrt(diskr)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(diskr)) / (2 * a);
+  } else if (diskr === 0) {
+    arr[0] = - b / (2 * a);
+  };
   return arr; // array
 }
 
